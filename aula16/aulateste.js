afcee7ed7cfbeaad0bf5ee517d8c88cd5f374f12
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2023-09-04 22:06:30 -0300

    atualização

---
+++ aula16/aulateste.js
@@ -0,0 +1,14 @@
+// ==> Exemplo 01 - Uso de Colchetes:
+var frutas16 = [ "🍍", "🍊", "🍎", "🍉", "🥭" ];
+console.log(frutas16[2]);
+// ==> Exemplo 02 - Array Object (Objeto Array):
+var fruta = ["🍍", "🍊", "🍎", "🍉", "🥭"];
+console.log(fruta[3]);
+// ==> Exemplo 03 - Adicionando mais strings com método 'push'
+var idiomas16 = ["Português", "Inglês", "Espanhol", "Francês"];
+// ==> Exemplo 04 - Identificar tamanho do array - método 'length'
+var idiomas12 = ["Português", "Inglês", "Espanhol", "Francês"];
+// ==> Exemplo 05 - Exemplo de Array com Spread Operator
+var listaNumeros12 = [0, 1, 2, 3, 4, 5];
+// ==> Exemplo 06 - Exemplo de Array com laço de iteração
+var linguagensArray16 = new Array("JavaScript", "Python", "PHP", "C#");
---
+++ aula16/aulateste.ts
@@ -0,0 +1,30 @@
+// ==> Exemplo 01 - Uso de Colchetes:
+
+let frutas16: string[] = ["🍍", "🍊", "🍎", "🍉", "🥭"];
+console.log(frutas16[2]);
+
+// ==> Exemplo 02 - Array Object (Objeto Array):
+
+let fruta: Array<string> = ["🍍", "🍊", "🍎", "🍉", "🥭"];
+console.log(fruta[3]);
+
+// ==> Exemplo 03 - Adicionando mais strings com método 'push'
+
+let idiomas16: Array<string> = ["Português", "Inglês", "Espanhol", "Francês"];
+
+// ==> Exemplo 04 - Identificar tamanho do array - método 'length'
+
+let idiomas12: Array<string> = ["Português", "Inglês", "Espanhol", "Francês"];
+
+// ==> Exemplo 05 - Exemplo de Array com Spread Operator
+
+let listaNumeros12 = [0, 1, 2, 3, 4, 5];
+
+// ==> Exemplo 06 - Exemplo de Array com laço de iteração
+
+let linguagensArray16: string[] = new Array(
+  "JavaScript",
+  "Python",
+  "PHP",
+  "C#"
+);
