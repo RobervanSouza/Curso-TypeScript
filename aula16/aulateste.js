3882278f014c1f8331b9e9a03a0425cb5db42128
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2023-09-05 21:47:41 -0300

    aula 16

--- aula16/aulateste.js
+++ aula16/aulateste.js
@@ -1,13 +1,17 @@
 // ==> Exemplo 01 - Uso de Colchetes:
-var frutas16 = [ "🍍", "🍊", "🍎", "🍉", "🥭" ];
+var frutas16 = ["🍍", "🍊", "🍎", "🍉", "🥭"];
 console.log(frutas16[2]);
 // ==> Exemplo 02 - Array Object (Objeto Array):
 var fruta = ["🍍", "🍊", "🍎", "🍉", "🥭"];
 console.log(fruta[3]);
 // ==> Exemplo 03 - Adicionando mais strings com método 'push'
 var idiomas16 = ["Português", "Inglês", "Espanhol", "Francês"];
+console.log(idiomas16);
+idiomas16.push('alemão');
+console.log(idiomas16);
 // ==> Exemplo 04 - Identificar tamanho do array - método 'length'
 var idiomas12 = ["Português", "Inglês", "Espanhol", "Francês"];
+console.log(idiomas12.length);
 // ==> Exemplo 05 - Exemplo de Array com Spread Operator
 var listaNumeros12 = [0, 1, 2, 3, 4, 5];
 // ==> Exemplo 06 - Exemplo de Array com laço de iteração
--- aula16/aulateste.ts
+++ aula16/aulateste.ts
@@ -11,15 +11,22 @@ console.log(fruta[3]);
 // ==> Exemplo 03 - Adicionando mais strings com método 'push'
 
 let idiomas16: Array<string> = ["Português", "Inglês", "Espanhol", "Francês"];
+console.log(idiomas16);
+idiomas16.push('alemão');
+console.log(idiomas16);
+
 
 // ==> Exemplo 04 - Identificar tamanho do array - método 'length'
 
-let idiomas12: Array<string> = ["Português", "Inglês", "Espanhol", "Francês"];
+let idiomas12: Array<string> = ["Português", "Inglês", "Espanhol", "Francês", "teste"];
 
+console.log(idiomas12.length);
 // ==> Exemplo 05 - Exemplo de Array com Spread Operator
 
 let listaNumeros12 = [0, 1, 2, 3, 4, 5];
-
+const listaNumeros22 =[6, 7, 8, 9, 10]
+listaNumeros12.push(...listaNumeros22)
+console.log(listaNumeros12)
 // ==> Exemplo 06 - Exemplo de Array com laço de iteração
 
 let linguagensArray16: string[] = new Array(
@@ -28,3 +35,11 @@ let linguagensArray16: string[] = new Array(
   "PHP",
   "C#"
 );
+function lacosFor(array:string[]) {
+  for (let i = 0; i < array.length; i++) {
+  
+    console.log(linguagensArray16[i])
+    
+  }
+}
+lacosFor(linguagensArray16);
